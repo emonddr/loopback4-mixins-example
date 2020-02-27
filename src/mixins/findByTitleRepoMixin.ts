d61dbb9b31284e07d5e052df1aa1f6681148e1d9
Dominique Emond <dremond@ca.ibm.com>
2020-02-27 10:46:07 -0500

    improve repository mixin

--- src/mixins/findByTitleRepoMixin.ts
+++ src/mixins/findByTitleRepoMixin.ts
@@ -2,23 +2,20 @@ import {
   Constructor,
 } from '@loopback/context';
 
-import {Model} from '@loopback/repository';
+import {Model,CrudRepository,Where} from '@loopback/repository';
 import {FindByTitleInterface} from './findByTitleInterface';
 
 /*
  * This function adds a new method 'findByTitle' to a repository class
  * where 'M' is a model and 'R' is DefaultCrudRepository
  */
-export function FindByTitleRepositoryMixin<M extends Model, R extends Constructor<any>>(superClass: R) {
+export function FindByTitleRepositoryMixin<M extends Model & {title: string}, R extends Constructor<CrudRepository<M>>>(superClass: R) {
 
   return class extends superClass implements FindByTitleInterface<M> {
 
     async findByTitle(title: string): Promise<(M)[]> {
-      const titleFilter = {
-        where: {
-          title: title
-        }
-      };
+      const where = {title} as Where<M>;
+      const titleFilter = {where};
       return await this.find(titleFilter);
     }
   }
